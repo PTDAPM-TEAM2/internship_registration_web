b0ea406ea4c69ff55ea272230c8f52b3ab80291a
Duc_Phong <90565925+ngducphong@users.noreply.github.com>
2023-03-06 15:38:17 +0700

    fix - 1 kỳ - 1 sinh viên chỉ có 1 đồ án

--- api/edu/src/main/java/com/group4/edu/repositories/GraduationThesisRepository.java
+++ api/edu/src/main/java/com/group4/edu/repositories/GraduationThesisRepository.java
@@ -1,10 +1,18 @@
 package com.group4.edu.repositories;
 
 import com.group4.edu.domain.GraduationThesis;
+import com.group4.edu.dto.GraduationThesisDto;
+import com.group4.edu.dto.SearchObjectDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface GraduationThesisRepository extends JpaRepository<GraduationThesis,Long> {
+    @Query("select new com.group4.edu.dto.GraduationThesisDto(e) from GraduationThesis e" +
+            " where e.registerTime.id = ?2 and e.student.id = ?1")
+    List<GraduationThesisDto> getAllBySVAndRegisterTime(Long IdSv, Long registerTimeId);
 }
--- api/edu/src/main/java/com/group4/edu/service/Impl/GraduationThesisServiceImpl.java
+++ api/edu/src/main/java/com/group4/edu/service/Impl/GraduationThesisServiceImpl.java
@@ -13,10 +13,15 @@ import com.group4.edu.repositories.StudentRepository;
 import com.group4.edu.service.GraduationThesisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Service
 public class GraduationThesisServiceImpl implements GraduationThesisService {
+    @PersistenceContext
+    EntityManager manager;
 
     @Autowired
     GraduationThesisRepository graduationThesisRepository;
@@ -32,6 +37,14 @@ public class GraduationThesisServiceImpl implements GraduationThesisService {
             throw new Exception("Hãy nhập thông tin");
         }
         GraduationThesis entity = null;
+        if(dto.getStudent() != null && dto.getStudent().getId() != null
+                && dto.getRegisterTime() != null && dto.getRegisterTime().getId() != null ){
+            List<GraduationThesisDto> graduationThesis = graduationThesisRepository.getAllBySVAndRegisterTime(dto.getStudent().getId(), dto.getRegisterTime().getId());
+            if(graduationThesis != null && graduationThesis.size() > 0){
+                throw new Exception("Sinh viên đã có đồ án");
+            }
+        }
+
         if(dto.getId() != null){
             entity = graduationThesisRepository.findById(dto.getId()).orElse(null);
         }
@@ -107,7 +120,23 @@ public class GraduationThesisServiceImpl implements GraduationThesisService {
 
     // cái này để lấy được danh sách đồ án và sinh viên
     public List<GraduationThesisDto> getGraduationThesis(SearchObjectDto dto){
+        String whereClause = " where true = true ";
+        String sql = "SELECT new com.group4.edu.dto.GraduationThesisDto(entity) FROM GraduationThesis as entity";
+//        sql += "  INNER JOIN Student s ON entity.student.id = s.id ";
 
-        return null;
+        if (dto.getStatus() != null) {
+//            sql += "  INNER JOIN Student s ON entity.student.id = s.id ";
+            whereClause += " AND (entity.status = :status)";
+        }
+
+        sql += whereClause;
+        Query query = manager.createQuery(sql, GraduationThesisDto.class);
+
+        if (dto.getStatus() != null) {
+            query.setParameter("status", dto.getStatus());
+        }
+
+        List<GraduationThesisDto> entities = query.getResultList();
+        return entities;
     }
 }
