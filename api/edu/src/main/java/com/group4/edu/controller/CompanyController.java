47268d238e50c735bbbb50151534fdcb83c27e38
LvNamm <luongvannam1308@gmail.com>
2023-03-18 13:45:25 +0700

    fix swagger

--- api/edu/src/main/java/com/group4/edu/controller/CompanyController.java
+++ api/edu/src/main/java/com/group4/edu/controller/CompanyController.java
@@ -2,22 +2,29 @@ package com.group4.edu.controller;
 
 import com.group4.edu.dto.CompanyDto;
 import com.group4.edu.service.CompanyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "Công ty")
 @RestController
 @RequestMapping("/api/company")
 public class CompanyController {
     @Autowired
     CompanyService companyService;
+    @ApiOperation(value = "Thêm công ty")
     @PostMapping("/save")
     public ResponseEntity<CompanyDto> save(@RequestBody CompanyDto dto){
         CompanyDto result = companyService.saveOrUpdate(dto);
         return ResponseEntity.ok(result);
     }
+
+    @ApiOperation(value = "Lấy danh sách công ty")
     @GetMapping("/getAll")
     public List<CompanyDto> getAll() {
         return companyService.getAll();
