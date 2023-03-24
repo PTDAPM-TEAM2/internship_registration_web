03a4e8004183d55a1ea0ded8f9b0c197c8c93c8e
TheBeginer2002 <quangphamxuan61@gmail.com>
2023-03-24 21:42:03 +0700

    update-pages

--- Form_Login/src/components/StudentRole/SVDA/DoAnSV/DeCuong/index.jsx
+++ Form_Login/src/components/StudentRole/SVDA/DoAnSV/DeCuong/index.jsx
@@ -2,26 +2,40 @@ import React, { useRef } from "react";
 import { Button, Box } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import graduationThesis from "../../../../../api/graduationThesis";
+// import { Document, Page,PDFViewer } from '@react-pdf/renderer';
 
 const DC = () => {
     const token = localStorage.getItem('token');
     const inputRef = useRef();
-    const [pdfFile, setPDFFile] = React.useState(null);
+    const [filePDF, setFilePDF] = React.useState(null);
+    const formData = new FormData();
     const handleClick = () => {
         inputRef.current.click();
     }
     const handleChange = (e) => {
-        console.log(e.target.files[0]);
-        setPDFFile(e.target.files[0]);
+        const files = e.target.files[0];
+        setFilePDF(files);
     }
+    formData.append('file', filePDF);
+    console.log(formData);
     const onSubmit = async () => {
         try {
-            const response = await graduationThesis.insertPDF(pdfFile,token);
+            const response = await graduationThesis.insertPDF({
+                "file": formData,
+            },token);
             console.log(response);
         } catch (error) {
             console.log(error);
         }
     }
+
+    // const MyDocument = ({ file }) => (
+    //     <Document file={file}>
+    //       <Page size="A4">
+    //       </Page>
+    //     </Document>
+    //   );
+    
     return (
         <>
             <Box sx={{ display: 'flex' }}>
@@ -35,21 +49,31 @@ const DC = () => {
                                 <h3>Đề cương của bạn:</h3>
                             </div>
                             <div style={{ width: '100%' }}>
-                                <div style={{ height: '90%', marginTop: 20, marginBottom: 180, textAlign: 'center' }}>
-                                <input
+                                <div style={{ height: '90%', marginTop: 20, marginBottom: 180, alignItems:"center",display:"flex",flexFlow:"column",justifyContent:"center" }}>
+                                <input 
                                     type="file"
                                     name="file"
+                                    accept=".pdf"
                                     hidden
                                     ref={inputRef}
                                     onChange={handleChange}
                                     />
-                                    <Button
+                                    { filePDF &&
+                                        <div style={{borderStyle: "solid", borderWidth: '2px',borderRadius:5,width:700,marginTop:10,textAlign:"center"}}>
+                                            <h4>{filePDF.name}</h4>
+                                        </div>
+                                        // <PDFViewer>
+                                        //     <MyDocument file={filePDF} />
+                                        // </PDFViewer>
+                                    }
+                                    <Button 
                                         onClick={handleClick}
                                         variant="outlined"
                                         color="inherit"
-                                        sx={{ width: 700, height: 120 }}
+                                        sx={{ width: 700, height: 120,textAlign:"center" }}
                                         startIcon={<AddIcon />}
                                     >Nộp đề cương</Button>
+                                    
                                 </div>
                                 <div style={{ height: '10%', }}>
                                     <Button variant="contained" color="success" onClick={onSubmit}>Lưu</Button>
