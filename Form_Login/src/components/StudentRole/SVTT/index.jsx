61bee0b0a1a1bdecca762a263c4af32d28017d16
Dootam21 <tam210402@gmail.com>
2023-03-25 21:32:33 +0700

    updating

--- Form_Login/src/components/StudentRole/SVTT/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const SVTT = () => {
-    return (
-        <div></div>
-    );
-}
-
-export default SVTT;
--- Form_Login/src/components/TeacherRole/pages/TrangChuGiangVien/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TCGV = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default TCGV
