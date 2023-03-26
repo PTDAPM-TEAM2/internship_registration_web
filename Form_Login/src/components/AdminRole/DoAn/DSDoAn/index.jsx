634a96a2a8cc49246489708906d10e9d7ec3adcd
tavanvinh172 <85282907+tavanvinh172@users.noreply.github.com>
2023-03-26 22:46:59 +0700

    export graduation thesis

--- Form_Login/src/components/AdminRole/DoAn/DSDoAn/index.jsx
+++ Form_Login/src/components/AdminRole/DoAn/DSDoAn/index.jsx
@@ -18,6 +18,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { useNavigate } from "react-router-dom";
 import prjApi from "../../../../api/projectApi";
+import companyApi from '../../../../api/companyApi.js';
 
 const style = {
     position: 'absolute',
@@ -95,6 +96,21 @@ function DSDA() {
         getAllDoAn();
     }, [])
 
+    const handleGo = async () => {
+        // data.filter((data) => item.id !== id);
+        try{
+            setOpen(false)
+            // await companyApi.exportGraduationThesis()
+        }catch(err){
+            console.log(err);
+        }
+        setTimeout(() => {
+            navigate('/danh-sach-sinh-vien-yeu-cau');
+        }, 1000)
+    }
+
+    console.log(projects.map((it) => console.log(it.semester)));
+
     return (
         <div style={{ display: 'flex' }}>
             <div className={styles.contain}>
@@ -158,9 +174,11 @@ function DSDA() {
                         id="demo-simple-select-helper"
                         onChange={handleChange}
                     >
-                        <MenuItem value="10">
-                            01/2022-2023
-                        </MenuItem>
+                        {projects.map(it => 
+                            <MenuItem value="10">
+                                {it.semester.code}
+                            </MenuItem>
+                            )}
                     </Select>
                     <div style={{ display: 'flex', justifyContent: 'space-around', paddingTop: 40 }}>
                         <Button className={styles.button} onClick={() => setOpen(false)}>Xuất</Button>
