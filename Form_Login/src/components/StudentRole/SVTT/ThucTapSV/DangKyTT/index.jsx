5911c73184b523385628487ff0130bea1618f75d
Dootam21 <tam210402@gmail.com>
2023-04-09 16:08:26 +0700

    fix register time in student role

--- Form_Login/src/components/StudentRole/SVTT/ThucTapSV/DangKyTT/index.jsx
+++ Form_Login/src/components/StudentRole/SVTT/ThucTapSV/DangKyTT/index.jsx
@@ -1,281 +1,4 @@
-// import React, { useContext } from "react";
-// import { Avatar, Button, TextField } from "@mui/material";
-// import { Box } from "@mui/system";
-// import userApi from "../../../../../api/authApi";
-// import * as yup from "yup";
-// import { Form, Formik, useFormik } from "formik";
-// import studentApi from "../../../../../api/studentApi";
-// import { ThemeContext } from "../../../../Theme/Theme";
-// import AlertMessage from "./Alert";
-// import styles from './DangKyTT.module.css';
 
-// const styleTextField = {
-//     width: '80%', marginBottom: 5
-// };
-
-// const validationSchema = yup.object().shape({
-//     nameCompany: yup.string().required("Vui lòng nhập tên công ty"),
-//     addressCompany: yup.string().required("Vui lòng nhập địa chỉ"),
-//     emailCompany: yup.string().email("email không hợp lệ").required("Vui lòng nhập email"),
-//     position: yup.string().required("Vui lòng nhập chức vụ"),
-//     phoneCompany: yup.string().required("Vui lòng nhập số điện thoại"),
-//     companyCode: yup.string().required("Vui lòng nhập mã công ty"),
-// });
-
-// const initialValues = {
-//     nameCompany: "",
-//     addressCompany: "",
-//     emailCompany: "",
-//     position: "",
-//     phoneCompany: "",
-//     companyCode: "",
-// };
-
-// const getTime = {
-//     timeStart: new Date(),
-//     timeEnd: new Date(),
-// }
-
-// function DKTT() {
-//     const token = localStorage.getItem('token');
-//     const [user, setUser] = React.useState([]);
-//     const [showAlert, setShowAlert] = React.useState(null);
-//     const context = useContext(ThemeContext)
-//     const [start, setStart] = React.useState('');
-//     const [end, setEnd] = React.useState('');
-//     const [companies, setCompanies] = React.useState([]);
-
-//     React.useEffect(() => {
-//         const getInterTime = async () => {
-//             try {
-//                 const response = await studentApi.getInternshipTime();
-//                 setStart(new Date(response.timeStart))
-//                 setEnd(new Date(response.timeEnd))
-//             } catch (err) {
-//                 console.log('Error fetching data', err);
-//             }
-//         };
-//         getInterTime();
-//         const getTTDASV = async () => {
-//             context.updateLoading(true);
-//             try {
-//                 const response = await userApi.getInfo(token);
-//                 setUser(response);
-//                 context.updateLoading(false);
-//                 console.log(response);
-//             } catch (error) {
-//                 context.updateLoading(false);
-//                 setShowAlert({ type: 'error', text: 'Có lỗi xảy ra' + error });
-//                 setTimeout(() => {
-//                     setShowAlert(null);
-//                 }, 2000)
-//             }
-//         };
-//         getTTDASV();
-
-
-//         // const getCompany = async () => {
-//         //     context.updateLoading(true);
-//         //     try {
-//         //         const response = await companyApi.getCompanies(token);
-//         //         setCompanies(response);
-//         //         context.updateLoading(false);
-//         //     } catch (error) {
-//         //         console.error('Error fetching data:', error);
-//         //         context.updateLoading(false);
-
-//         //     }
-//         // }
-//         // getCompany()
-//     }, []);
-
-
-//     const handleSubmit = async (values) => {
-
-//         const dateNow = new Date();
-//         if (dateNow > end) {
-//             alert("Thời gian đăng ký đã kết thúc");
-//         } else {
-//             context.updateLoading(true);
-//             try {
-//                 const response = await studentApi.internshipRegisterBySv({
-//                     "studentCode": null,
-//                     "studentId": null,
-//                     "code": null,
-//                     "nameCompany": values.nameCompany,
-//                     "address": values.addressCompany,
-//                     "email": values.emailCompany,
-//                     "internshipPosition": values.position,
-//                     "phoneNumber": values.phoneCompany,
-//                     "taxCode": values.companyCode,
-//                     "description": null,
-//                 }, token);
-//                 context.updateLoading(false);
-//                 setShowAlert({ type: 'success', text: "Thêm sinh viên thành công" });
-//                 setTimeout(() => {
-//                     setShowAlert(null);
-//                 }, 2000)
-//                 console.log(response);
-//             } catch (error) {
-//                 context.updateLoading(false);
-//                 setShowAlert({ type: 'error', text: "Thêm sinh viên không thành công " + error });
-//                 setTimeout(() => {
-//                     setShowAlert(null);
-//                 }, 2000)
-//             }
-//         }
-//     }
-//     const formik = useFormik({
-//         initialValues,
-//         validationSchema,
-//         onSubmit: handleSubmit,
-//     })
-// return (
-//         <>
-//             <AlertMessage message={showAlert} />
-//             <Formik>
-//                 <Box sx={{ display: 'flex' }}>
-//                     <div style={{ display: "block", borderStyle: "solid", borderWidth: '2px', top: '10%', width: '100%', height: 720, marginTop: 30, marginLeft: 8, marginRight: 8 }}>
-//                         <div style={{ borderBottom: '2px ', width: '100%', height: '65%', boxSizing: 'border-box' }}>
-//                             <div style={{ height: '12%', borderBottom: '2px solid', textAlign: 'center', backgroundColor: "lightgrey" }}>
-//                                 <h1 style={{ fontWeight: 700, fontSize: 25, }}><b>Đăng ký thực tập:</b></h1>
-//                             </div>
-//                             <div style={{ height: '88%', borderBottom: '2px', marginTop: 10, textAlign: 'left', marginLeft: 20, }}>
-//                                 <div style={{ float: 'left', width: '20%', height: '100%' }}>
-//                                     <Avatar src={user?.urlImg || "https://cdn-icons-png.flaticon.com/512/149/149071.png"} sx={{ width: 161, height: 161, marginBottom: 5 }}></Avatar>
-//                                     <h4 style={{ marginBottom: 10 }}>Giới tính:</h4>
-//                                     <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={{ width: '80%', }} value={user?.gender || ""} />
-//                                 </div>
-//                                 <div style={{ float: 'left', width: '40%', height: '100%', }}>
-//                                     <h4 style={{ marginBottom: 10 }}>Họ và tên:</h4>
-//                                     <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={styleTextField} value={user?.fullName || ""} />
-//                                     <h4 style={{ marginBottom: 10 }}>Ngày sinh:</h4>
-//                                     <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={styleTextField} />
-//                                     <div style={{ width: "100%" }}>
-//                                         <div style={{ float: 'left', }}>
-//                                             <h4 style={{ marginBottom: 10 }}>Số điện thoại:</h4>
-//                                             <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={{ width: '100%', marginBottom: 5 }} value={user?.phoneNumber || ""} />
-//                                         </div>
-//                                         <div style={{ position: 'relative', right: '-25%' }}>
-//                                             <h4 style={{ marginBottom: 10 }}>Khoa:</h4>
-//                                             <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={{ width: '20%', marginBottom: 5 }} value={user?.grade?.name || ""} />
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div style={{ float: 'left', width: '40%', height: '100%' }}>
-//                                     <h4 style={{ marginBottom: 10 }}>Nơi sinh:</h4>
-//                                     <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={styleTextField} value={user?.placeOfBirth || ""} />
-//                                     <h4 style={{ marginBottom: 10 }}>Email:</h4>
-//                                     <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={styleTextField} value={user?.email || ""} />
-//                                     <div style={{ width: "100%" }}>
-//                                         <div style={{ float: 'left' }}>
-//                                             <h4 style={{ marginBottom: 10 }}>Mã sinh viên:</h4>
-//                                             <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={{ width: '100%', marginBottom: 5 }} value={user?.studentCode || ""} />
-//                                         </div>
-//                                         <div style={{ position: 'relative', right: '-25%' }}>
-//                                             <h4 style={{ marginBottom: 10 }}>Lớp:</h4>
-//                                             <TextField disabled id="outlined-basic" variant="outlined" size="small" sx={{ width: '20%', marginBottom: 5 }} value={user?.grade?.name || ""} />
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div style={{ borderTop: '2px groove', width: '100%', height: '35%', boxSizing: 'border-box' }}>
-//                             <div style={{ display: 'block', height: '100%' }}>
-//                                 <div style={{ width: "100%", height: '100%', textAlign: 'left', marginLeft: 20, marginRight: 10 }}>
-//                                     <div style={{ float: 'left', width: '33%', height: '100%' }}>
-//                                         <h4 style={{ marginBottom: 10 }}>Tên công ty:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="nameCompany"
-//                                             value={formik.values.nameCompany}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.nameCompany && Boolean(formik.errors.nameCompany)}
-//                                             helperText={formik.touched.nameCompany && formik.errors.nameCompany} />
-//                                         <h4 style={{ marginBottom: 10 }}>Địa chỉ công ty:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="addressCompany"
-//                                             value={formik.values.addressCompany}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.addressCompany && Boolean(formik.errors.addressCompany)}
-//                                             helperText={formik.touched.addressCompany && formik.errors.addressCompany} />
-//                                     </div>
-//                                     <div style={{ float: 'left', width: '33%', height: '100%' }}>
-//                                         <h4 style={{ marginBottom: 10 }}>Email công ty:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="emailCompany"
-//                                             value={formik.values.emailCompany}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.emailCompany && Boolean(formik.errors.emailCompany)}
-//                                             helperText={formik.touched.emailCompany && formik.errors.emailCompany} />
-//                                         <h4 style={{ marginBottom: 10 }}>Vị trí thực tập:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="position"
-//                                             value={formik.values.position}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.position && Boolean(formik.errors.position)}
-//                                             helperText={formik.touched.position && formik.errors.position} />
-//                                     </div>
-//                                     <div style={{ float: 'left', width: '33%', height: '100%' }}>
-//                                         <h4 style={{ marginBottom: 10 }}>SĐT công ty:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="phoneCompany"
-//                                             value={formik.values.phoneCompany}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.phoneCompany && Boolean(formik.errors.phoneCompany)}
-//                                             helperText={formik.touched.phoneCompany && formik.errors.phoneCompany} />
-//                                         <h4 style={{ marginBottom: 10 }}>Mã công ty:</h4>
-//                                         <TextField
-//                                             id="outlined-basic"
-//                                             variant="outlined"
-//                                             size="small"
-//                                             sx={{ width: '80%', marginBottom: 5 }}
-//                                             name="companyCode"
-//                                             value={formik.values.companyCode}
-//                                             onChange={formik.handleChange}
-//                                             onBlur={formik.handleBlur}
-//                                             error={formik.touched.companyCode && Boolean(formik.errors.companyCode)}
-//                                             helperText={formik.touched.companyCode && formik.errors.companyCode} />
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div style={{ textAlign: "right", }}>
-//                             <Button variant="contained" style={{ backgroundColor: '#23434E' }} type="submit" onClick={handleSubmit}>Đăng ký</Button>
-//                         </div>
-//                     </div>
-//                 </Box>
-//             </Formik>
-//         </>
-//     );
-
-// }
-
-// export default DKTT;
 import * as React from 'react';
 import { TextField } from '@mui/material';
 import styles from './DangKyTT.module.css';
@@ -342,7 +65,8 @@ const DKTT = () => {
         const getInterTime = async () => {
             try {
                 const response = await studentApi.getInternshipTime();
-                // s
+                setStart(response.timeStart)
+                setEnd(response.timeEnd);
             } catch (err) {
                 console.log('Error fetching data', err);
             }
@@ -370,11 +94,13 @@ const DKTT = () => {
         validationSchema: validationSchema,
         onSubmit: async (values) => {
             context.updateLoading(true);
-            if (getToday < start) {
+            if (getToday < start || start === null || end === null) {
                 setErrorMessages("Chưa đến thời gian đăng ký thực tập!");
+                context.updateLoading(false);
             }
             else if (getToday > end) {
                 setErrorMessages("Đã hết thời gian đăng ký thực tập!");
+                context.updateLoading(false);
             }
             else {
                 try {
@@ -524,7 +250,7 @@ const DKTT = () => {
                             </div>
                         </div>
                         <div className={styles.btn}>
-                            <button className={styles.button} type="submit">Thêm</button>
+                            <button className={styles.button} type="submit" >Thêm</button>
                         </div>
                     </form>
                 </div>
